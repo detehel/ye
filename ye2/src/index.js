60d006f49ab3f25d96864c2fd10bfa9441474499
detehel <90442141+detehel@users.noreply.github.com>
2021-11-22 23:47:10 +0200

    Add files via upload

---
+++ ye2/src/index.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import reportWebVitals from './reportWebVitals';
+
+const myelement = (
+  <table>
+    <tr>
+      <td>
+  <div class="header">
+    <a href="index.html">
+      <img src="logo_trans_shadow.png" alt="ye, gimme a sign" width="300" height="auto" /> </a>
+  </div>
+</td>
+</tr>
+</table>
+);
+
+    var r_text = new Array (); 
+    r_text[0] = "Now if I fuck this model <br> And she just bleached her asshole <br> And I get bleach on my T-shirt <br> I’mma feel like an asshole <br>  <br>  <br> " + "<h6>Father Stretch My Hands Pt. 1 "; 
+    r_text[1] = "Never in your wildest dreams <br> Never in your wildest dreams <br> In your wildest <br> You can hear the loudest screams <br> Comin' from inside the screen <br> You a wild bitch <br> Tell me what I gotta do to be that guy <br> Said her price go down, she ever fuck a black guy <br> Or do anal, or do a gang bang <br> It's kinda crazy, that's all considered the same thing <br> Well I guess a lot of niggas do gang bang <br> And if we run trains, we all in the same gang <br> Runaway slaves all on a chain gang <br> Bang, bang, bang, bang, bang <br> <br>  <br> " + "<h6> Hell of a Life";
+    r_text[2] = "I think I just fell in love with a porn star <br> Turn the camera on, she a born star <br>  <br>  <br> " + "<h6> Hell of a Life"; 
+    r_text[3] = "Have you lost your mind? <br> Tell me when you think we've crossed the line <br> No more drugs for me <br> Pussy and religion is all I need <br> Grab my hand and <br> Baby we'll live a hell of a life <br>  <br>  <br> " + "<h6> Hell of a Life"; 
+    r_text[4] = "One day I'm gon' marry a porn star <br> We'll have a big ass crib and a long yard <br> We'll have a mansion and some fly maids <br> Nothin' to hide, we both screwed the bridesmaids <br> She wanna roll play, 'til I roll over <br> I'ma need a whole day, at least roll doja <br>  <br>  <br> " + "<h6> Hell of a Life";
+    r_text[5] = "How could you say they live they life wrong? <br> When you never fuck wit' the lights on <br>  <br>  <br> " + "<h6> Hell of a Life";  
+    r_text[6] = "I think I fell in love with a porn star <br> And got married in the bathroom <br> Honeymoon on the dance floor <br> And got divorced by the end of the night <br> That's one hell of a life <br>  <br>  <br> " + "<h6> Hell of a Life"; 
+    r_text[7] = "I love your titties 'cause they prove I can focus on two things at once <br>  <br>  <br> " + "<h6> All Mine"; 
+    r_text[8] = "We at war <br> We at war with terrorism, racism <br> But most of all we at war with ourselves <br>  <br>  <br> " + "<h6> Jesus Walks"; 
+    r_text[9] = "Waiting patiently; I ask myself <br> Where I wanna go, where I wanna be <br> Life is much more than running in the streets <br>  <br>  <br> " + "<h6> Spaceship"; 
+    r_text[10] = "We buy our way out of jail, but we can't buy freedom <br>  <br>  <br> " + "<h6> All Falls Down"; 
+    r_text[11] = "The drug game bulimic, it's hard to get weight  <br> So niggas' money is homo, it's hard to get straight <br>  <br>  <br> " + "<h6> We Don’t Care"; 
+    r_text[12] = "Drug dealin' just to get by  <br> Stack your money 'til it get sky high <br>  <br>  <br> " + "<h6> We Don’t Care"; 
+    r_text[13] = "'Cause ain't no tuition for having no ambition  <br> And ain't no loans for sitting your ass at home <br>  <br>  <br> " + "<h6> We Don’t Care"; 
+    r_text[14] = "We claim other people kids on our income tax  <br> We take that money, cop work, then push packs to get paid  <br> And we don't care what people say <br>  <br>  <br> " + "<h6> We Don’t Care"; 
+    r_text[15] = "You know the kids gon' act a fool <br> When you stop the programs for after school  <br> And they DCFS, some of 'em dyslexic  <br> They favorite 50 Cent song '12 Questions'  <br>  <br>  <br> " + "<h6> We Don’t Care"; 
+    r_text[16] = "Couldn't afford a car, so she named her daughter Alexis <br>  <br>  <br> " + "<h6> All Falls Down"; 
+    r_text[17] = "God show me the way because the Devil's tryna break me down <br> (Jesus walk with me) <br> The only thing that I pray is that my feet don't fail me now <br>  <br>  <br> " + "<h6> Jesus Walks"; 
+    r_text[18] = "When it comes to being true, at least true to me <br> One thing I've found, one thing I found <br> Oh no, you'll never let me down <br> Get up, I get down, get up, I get down <br>  <br>  <br> " + "<h6> Never Let Me Down"; 
+    r_text[19] = "Racism's still alive, they just be concealin' it <br>  <br>  <br> " + "<h6> Never Let Me Down"; 
+    r_text[20] = "We are all here for a reason on a particular path <br> You don't need a curriculum to know that you're a part of the math <br> Cats think I'm delirious but I'm so damn serious <br> That's why I expose my soul to the globe; the world <br> I'm trying to make it better for these little boys and girls <br> I'm not just another individual <br> My spirit is a part of this that's why I get spiritual <br> But I get my hymns from Him <br> So it's not me, it's He that's lyrical <br> I'm not a miracle, I'm a heaven-sent instrument <br> My rhythmatic regiment navigates melodic notes <br> For your soul and your mental <br>  <br>  <br> " + "<h6> Never Let Me Down"; 
+    r_text[21] = "I too dream in color and in rhyme <br> So I guess I'm one of a kind in a full house <br> 'Cause whenever I open my heart, my soul or my mouth <br> A touch of God reigns out <br> Take 'em to church <br>  <br>  <br> " + "<h6> Never Let Me Down"; 
+    r_text[22] = "Determination, dedication, motivation <br> I'm talking to you of my many inspirations <br> When I say I can't let you or self-down <br> If I were on the highest cliff, on the highest riff <br> And if you slipped off the side and clinched on to your life <br> In my grip, I would never ever let you down <br>  <br>  <br> " + "<h6> Never Let Me Down"; 
+    r_text[23] = "Real rappers is hard to find, like a remote <br>  <br>  <br> " + "<h6> Get Em High"; 
+    r_text[24] = "You just popped in the Kanye West <br> Get Right for the Summer workout tape <br> And ladies if you follow these instructions exactly <br> You might be able to pull you a rapper, a NBA player <br> Man, at least a dude with a car <br>  <br>  <br> " + "<h6> The New Workout Plan"; 
+    r_text[25] = "1, and 2, and 3, and 4, and get them sit ups right, and <br> Tuck your tummy tight and do your crunches like this <br> Give head, stop, breathe, get up, check your weave <br> Don't drop the blunt and disrespect the weed <br> Pick up your son and don't disrespect your seed <br>  <br>  <br> " + "<h6> The New Workout Plan"; 
+    r_text[26] = "It's a party tonight and ooh she's so excited <br> Tell me who's invited: you, your friends and my dick <br>  <br>  <br> " + "<h6> The New Workout Plan"; 
+    r_text[27] = "Henny makes girls look like Halle Berry to me <br> So excuse me miss, I forgot your name <br> Thank you, God bless you, good night I came, I came, I came <br>  <br>  <br> " + "<h6> The New Workout Plan"; 
+    r_text[28] = "Ooh girl your breath is harsh! <br> Cover your mouth up like you got SARS <br>  <br>  <br> " + "<h6> The New Workout Plan"; 
+    r_text[29] = "I told her to drive over in your new whip <br> Bring some friends you cool with <br> I'm gonna bring the cool whip <br> Then I want you to strip <br> See you is my new chick <br> So we get our grind on <br>  <br>  <br> " + "<h6> Slow Jamz"; 
+    r_text[30] = "Telling me them diamonds when she know they rhinestones <br>  <br>  <br> " + "<h6> Slow Jamz"; 
+    r_text[31] = "She got a light skinned friend look like Michael Jackson <br> Got a dark skinned friend look like Michael Jackson <br>  <br>  <br> " + "<h6> Slow Jamz"; 
+    r_text[32] = "I played 'Ready for the World' - she was ready for some action <br> My dog said you ain't no freak, so you're 'bout to prove my man wrong <br> I'm a play this Vandross <br> You're gonna take your pants off <br> I'm gonna play this Gladys Knight <br> Me and you're gonna get right <br>  <br>  <br> " + "<h6> Slow Jamz"; 
+    r_text[33] = "Girls go wild and pull ya D's out  <br>  Breathe in, breathe out  <br>  Let them hoes fight, pull her weave out  <br>  If a nigga act up, pull a Desert E's out  <br>  When I pull the piece out niggas like 'Peace out!' <br>  <br>  <br> " + "<h6> Breathe In Breathe Out"; 
+    r_text[34] = "Now even though I went to college  <br>  And dropped out of school quick  <br>  I always had a Ph.D: a pretty huge dick <br>  <br>  <br> " + "<h6> Breathe In Breathe Out"; 
+    r_text[35] = "Coulda sworn her breasteses was sendin' me messages  <br>  'K I need a free hand mammogram  <br>  I got weed, drink, and a Handicam  <br>  All of which is legal in Amsterdam' <br>  <br>  <br> " + "<h6> Breathe In Breathe Out"; 
+    r_text[36] = "Told 'em I finished school and I started my own business  <br>  They say, 'Oh you graduated?' No, I decided I was finished  <br>  Chasing y'all dreams and what you've got planned <br>  <br>  <br> " + "<h6> School Spirit"; 
+    r_text[37] = " We in the streets, playa, get ya mail <br> It's only two places you end up, either dead or in jail <br> Still nowhere to go<br>  <br>  <br> " + "<h6> Two Words"; 
+    r_text[38] = "So I live by two words: 'Fuck you, pay me' <br>  <br>  <br> " + "<h6> Two Words"; 
+    r_text[39] = "For a chance to be with you, I'd gladly risk it all <br> Through the fire, through whatever, come what may <br> For a chance at loving you, I'd take it all the way <br>  <br>  <br> " + "<h6> Through The Wire"; 
+    r_text[40] = "Thank God I ain't too cool for the safe belt <br> I swear to God, driver two wanna sue <br> I got a lawyer for the case, to keep what's in my safe safe <br>  <br>  <br> " + "<h6> Through The Wire"; 
+    r_text[41] = "They thought I was burnt up like Pepsi did Michael <br>  <br>  <br> " + "<h6> Through The Wire"; 
+    r_text[42] = "Good dude, bad night, right place, wrong time <br> In the blink of a eye, his whole life changed <br>  <br>  <br> " + "<h6> Through The Wire"; 
+    r_text[43] = "Keep your nose out the sky, keep your heart to God <br> And keep your face to the risin' sun <br>  <br>  <br> " + "<h6> Family Business"; 
+    r_text[44] = "My money was thinner than Sean Paul's goatee hair <br>  <br>  <br> " + "<h6> Last Call"; 
+    r_text[45] = "Flow infectious, give me 10 seconds <br> I'll have a buzz bigger than insects in Texas <br> It's funny how wasn't nobody interested <br> 'Til the night I almost killed myself in Lexus <br>  <br>  <br> " + "<h6> Last Call"; 
+    r_text[46] = "Girl he had with him - ass coulda won the horse awards <br>  <br>  <br> " + "<h6> Last Call"; 
+    r_text[47] = "'Oh my god, is that a black card?' <br> I turned around and replied, 'Why yes <br> But I prefer the term African American Express' <br>  <br>  <br> " + "<h6> Last Call"; 
+    r_text[48] = "They say people in your life are seasons <br> And anything that happen is for a reason <br>  <br>  <br> " + "<h6> Heard 'Em Say"; 
+    r_text[49] = "With every worthless word we get more far away <br> And nothing's ever promised tomorrow today <br> And nothing lasts forever but be honest babe <br> It hurts but it may be the only way <br>  <br>  <br> " + "<h6> Heard 'Em Say"; 
+    r_text[50] = "I think I died in that accident, 'cause this must be Heaven <br>  <br>  <br> " + "<h6> Touch The Sky"; 
+    r_text[51] = "She take my money when I'm in need <br> Yeah, she's a triflin' friend indeed <br> Oh, she's a gold digger <br> Way over town that digs on me <br>  <br>  <br> " + "<h6> Gold Digger"; 
+    r_text[52] = "Now, I ain't sayin' she a gold digger <br> But she ain't messin' with no broke niggas <br>  <br>  <br> " + "<h6> Gold Digger"; 
+    r_text[53] = "Eighteen years, eighteen years <br> She got one of your kids, got you for eighteen years <br> I know somebody payin' child support for one of his kids <br> His baby mama car and crib is bigger than his <br> You will see him on TV any given Sunday <br> Win the Super Bowl and drive off in a Hyundai <br> She was supposed to buy your shorty Tyco with your money <br> She went to the doctor, got lipo with your money <br> She walkin' around lookin' like Michael with your money <br> Should've got that insured, Geico for your money <br> If you ain't no punk <br> Holla, 'We want prenup! We want prenup!'(Yeah!) <br> It's somethin' that you need to have <br> 'Cause when she leave yo' ass, she gon' leave with half <br> Eighteen years, eighteen years <br> And on the 18th birthday, he found out it wasn't his? <br>  <br>  <br> " + "<h6> Gold Digger"; 
+    r_text[54] = "Now, I ain't sayin' you're a gold digger, you got needs <br> You don't want a dude to smoke, but he can't buy weed <br> You go out to eat, he can't pay, y'all can't leave <br> There's dishes in the back, he gotta roll up his sleeves <br> But while y'all washin', watch him <br> He gon' make it to a Benz out of that Datsun <br> He got that ambition, baby, look at his eyes <br> This week he moppin' floors, next week it's the fries <br> So stick by his side <br>  <br>  <br> " + "<h6> Gold Digger"; 
+    r_text[55] = "I know there's dudes ballin', and yeah, that's nice <br> And they gonna keep callin' and tryin', but you stay right, girl <br> And when he get on, he'll leave yo' ass for a white girl <br>  <br>  <br> " + "<h6> Gold Digger"; 
+    r_text[56] = "Drive slow, homie <br> Drive slow, homie <br> You never know, homie, might meet some hoes, homie <br> You need to pump your brakes and drive slow, homie  <br>  <br>  <br> " + "<h6> Drive Slow"; 
+    r_text[57] = "With the girls a lot of flirtin' involved, but dog <br> Fuck all that flirtin' now I'm tryin' to get in some drawers <br> So put me on with these hoes, homie <br> He told me don't rush to get grown, drive slow, homie <br>  <br>  <br> " + "<h6> Drive Slow"; 
+    r_text[58] = "Turn your hazard lights on when you see them hoes <br> Drive slow, homie <br> If you're riding around the city with nowhere to go <br> Drive slow, homie <br> Live today, cause tomorrow, man, you never know <br> You never know, homie, might meet some hoes, homie <br> You need to pump your brakes and drive slow, homie <br>  <br>  <br> " + "<h6> Drive Slow"; 
+    r_text[59] = "I know it's past visiting hours <br> But can I please give her these flowers? <br> The doctor don't wanna take procedures <br> He claim her heart can't take the anaesthesia <br> It'll send her body into a seizure <br> That little thing by the hospital bed, it'll stop beeping <br>  <br>  <br> " + "<h6> Roses"; 
+    r_text[60] = "I asked the nurse, 'Did you do the research?' <br> She asked me, 'Can you sign some T-shirts?'  <br> Bitch, is you smoking reefer? <br> You don't see that we hurt?  <br>  <br>  <br> " + "<h6> Roses"; 
+    r_text[61] = "I think we at a all-time high <br> To get there, we run, we fly, we drive <br> 'Cause with my family we know where home is <br> And so instead of sending flowers, we the roses <br> I smile, when roses comes to see me <br> And I can't wait for a sunny day (Seeing it through your eyes) <br> Can't wait for the clouds to break <br>  <br>  <br> " + "<h6> Roses"; 
+    r_text[62] = "There'll always be haters, that's the way it is  <br> Hater niggas marry hater bitches and have hater kids <br>  <br>  <br> " + "<h6> Bring Me Down"; 
+    r_text[63] = "You see, if you ever wanted to ever be anything <br> There'd always be somebody that shoot down any dream <br>  <br>  <br> " + "<h6> Bring Me Down"; 
+    r_text[64] = "It's funny how these wack niggas need my help <br> Wasn't around when I couldn't feed myself <br> Dawg, if I was you, I wouldn't feel myself <br> Dawg, if I was you, I'd kill myself! <br> Made a mil' myself and I'm still myself <br> And I'mma look in the mirror if I need some help <br>  <br>  <br> " + "<h6> Bring Me Down"; 
+    r_text[65] = "Why everything that's supposed to be bad make me feel so good? <br> Everything they told me not to is exactly what I would <br>  <br>  <br> " + "<h6> Addiction"; 
+    r_text[66] = "What's your addiction? Is it money? Is it girls? Is it weed? <br> I've been afflicted by not one, not two, but all three <br> She's got the same thing, about me, but more, about us <br> She's coming over, so I guess, that means, I'm her drugs <br>  <br>  <br> " + "<h6> Addiction"; 
+    r_text[67] = "She's got a lover, so the lies, and the lust, is a rush <br> Time's of the essence, I need, you to be spontaneous <br> Roll up the doja, Henny and Co-co-coca-Cola <br> Then I'm co-coming over 'cause it's ne-never over <br>  <br>  <br> " + "<h6> Addiction"; 
+    r_text[68] = "I see the emotion in your eyes, that you, try not to show <br> We get the closest when you high, or you drunk, or you blown <br>  <br>  <br> " + "<h6> Addiction"; 
+    r_text[69] = "There's no turning back now, I mean I don't mean to impose <br> Not now but right now, I need you to undress, and then pose <br> I'm into that now, catch a vibe, when the doors, get closed <br>  <br>  <br> " + "<h6> Addiction"; 
+    r_text[70] = "Little was known of Sierra Leone <br> And how it connect to the diamonds we own <br>  <br>  <br> " + "<h6> Diamonds From Sierra Leone"; 
+    r_text[71] = "How can somethin' so wrong make me feel so right? <br>  <br>  <br> " + "<h6> Diamonds From Sierra Leone"; 
+    r_text[72] = "I had to get off the boat so I could walk on water <br>  <br>  <br> " + "<h6> Diamonds From Sierra Leone"; 
+    r_text[73] = "Difficult takes a day, impossible takes a week <br>  <br>  <br> " + "<h6> Diamonds From Sierra Leone"; 
+    r_text[74] = "Spoil yourself, that's important <br> If your stripper name 'Porsche' and you get tips from many men <br> Then your fat friend, her nickname is 'Minivan' <br> Excuse me, that's just the Henny, man <br> I smoke, I drink, I'm supposed to stop, I can't <br>  <br>  <br> " + "<h6> Diamonds From Sierra Leone"; 
+    r_text[75] = "<h6> (more advice coming soon) <br>  <br>  <br> " + "<h6> "; 
+    
+    var i = Math.floor(r_text.length * Math.random()); 
+    
+    document.write(r_text[i]); 
+    
+    var bgcolorlist=new Array("#090808", "#111F9A", "#b25186", "#f0e6d3", "#b9b353", "#dfa417", "#411218", "#b72124", "#f58b57", "#7acec2", "#cb2436", "#284061", "#ae6e1f", "#a89c9a", "#de6656", "#e6dfcc", "#412517", "#e5dab7", "#cb2436", "#853082", "#221e1f","#40b16d", "#2c4454", "#cd0f29") 
+    
+    document.body.style.background=bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]; 
+
+
+ReactDOM.render(myelement, document.getElementById('root'));
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
